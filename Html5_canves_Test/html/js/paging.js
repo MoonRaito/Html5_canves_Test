5e66233cd3e5b73d73d9e85e413f4696693bb496
MoonRaito <394950240@qq.com>
2016-02-02 11:25:38 +0800

    2016年02月02日11:25:25 添加 svg

--- Html5_canves_Test/html/js/paging.js
+++ Html5_canves_Test/html/js/paging.js
@@ -13,6 +13,10 @@ function map02Html()
 {
     window.location.href="map01.html";
 }
+function svg01Html()
+{
+    window.location.href="svg01.html";
+}
 function backMainHtml()
 {
     window.history.back(-1);
